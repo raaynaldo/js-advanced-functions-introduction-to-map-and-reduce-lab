6bbfda73f06533c26aec372d0d65167acb7f3481
raaynaldo <raaynaldo@hotmail.com>
2020-11-27 00:29:04 -0500

    commit tree bfc3caac80fb98ea047f395ca94546d7e5f6bbfc

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function mapToNegativize(nums) {
+  return nums.map(function (num) {
+    return num * -1;
+  });
+}
+
+function mapToNoChange(names) {
+  return names.map(function (name) {
+    return name;
+  });
+}
+
+function mapToDouble(nums) {
+  return nums.map(function (num) {
+    return num * 2;
+  });
+}
+
+function mapToSquare(nums) {
+  return nums.map(function (num) {
+    return num * num;
+  });
+}
+
+function reduceToTotal(nums, start = 0) {
+  return nums.reduce(function (start, num) {
+    return start + num;
+  }, start);
+}
+
+function reduceToAllTrue(sourceArray) {
+  return sourceArray.reduce(function (accumulator, value) {
+    return accumulator && !!value;
+  }, true);
+}
+
+function reduceToAnyTrue(sourceArray) {
+  return sourceArray.reduce(function (accumulator, value) {
+    return accumulator || !!value;
+  }, false);
+}
